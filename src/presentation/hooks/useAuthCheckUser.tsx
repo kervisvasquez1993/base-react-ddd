5a22564497141e2a1122abca7398cc145ac8bcf4
kervisvasquez1993 <kervisvasquez24@gmail.com>
2024-10-27 17:33:15 -0300

    update espacios

--- src/presentation/hooks/useAuthCheckUser.tsx
+++ src/presentation/hooks/useAuthCheckUser.tsx
@@ -6,12 +6,10 @@ const useAuthCheckUser = () => {
     const token = localStorage.getItem('access_token')
     const username = localStorage.getItem('username')
     const role = localStorage.getItem('role')
-
     if (token && username && role) {
       setIsLogged(true)
     }
   }, [])
-
   return isLogged
 }
 export default useAuthCheckUser
